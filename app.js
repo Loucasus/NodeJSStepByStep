ca2045aca41aa2f2bac3b372e3d59fee80c50605
Lucas Suau <suau.lucas@gmail.com>
2016-10-04 11:29:03 +0200

    Initialisation du depot2

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict'
+
 console.log("It Works !");
 
 var express = require("express");
@@ -11,10 +13,11 @@ var defaultRoute = require("./app/routes/default.route.js");
 app.use(defaultRoute);
 
 // #2
+/*
 app.get("/",function(request, response,cb) {
 
 	response.send("It works !");
-});
+});*/
 
 // #3
 app.use(function(request, response, cb){
---
+++ app/routes/default.route.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var  express  =  require("express");
+var  router  =  express.Router();
+module.exports  =  router;
+// Routing using
+/*
+router.route(__PATH__)
+    .get()
+    .post()
+    .put()
+    .delete()
+    .all()
+    .[...]
+*/
